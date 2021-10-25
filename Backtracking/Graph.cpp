767e939d8f089c042b317bfaae1e9bbd9e3f8969
marcelo <machotm@hotmail.com>
2021-10-25 00:32:29 -0600

    Backtracking funcional y funciones para propiciar el resultado

--- Backtracking/Graph.cpp
+++ Backtracking/Graph.cpp
@@ -5,6 +5,7 @@
 #include "Graph.h"
 
 Graph::Graph(int size) {
+    this->number=0;
     int i=0;
     while(i!=size){
         NodeLogistic newNode(i+1);
@@ -15,11 +16,12 @@ Graph::Graph(int size) {
 }
 
 void Graph::Adminconections(int index) {
-    int repetition=0;
-    int firstconection=rand()%this->Nodes.size();//índice de la primera conexión del nodo
     if (this->Nodes.size()==1){
         //No hay nodos con los que conectar
     }else{
+        int repetition=0;
+        int limit=this->Nodes.size()-1;
+        int firstconection=rand()%limit;//índice de la primera conexión del nodo
         while (repetition<=this->Nodes.size()){
             int peso=rand()%25+1;//Peso entre 1-25
             if (repetition==0){//Asegura que un nodo esté conectado mínimo a un nodo
@@ -51,7 +53,6 @@ void Graph::makeconnection(int Node1, int Node2, int peso) {
     this->Nodes.at(Node2).makeconnection(Node1+1,peso);
 }
 
-
 NodeLogistic Graph::searchnode(int ID) {
     for(auto i=this->Nodes.begin(); i!=this->Nodes.end();i++){
         if(i->getid()==ID){
@@ -62,5 +63,72 @@ NodeLogistic Graph::searchnode(int ID) {
     }
 }
 
+string Graph::Short(int Init, int Fin) {
+    vector<int> Initialvector;
+    Initialvector.push_back(Init);
+    this->resetbacktracking(Fin);
+    this->backtracking(--Init,--Fin,Initialvector);
+    this->Nodes.at(Fin).path.push_back(Fin+1);
+    string Answer=this->StringifyAnswer(this->Nodes.at(Fin).path);
+    return Answer;
+}
 
+string Graph::StringifyAnswer(vector<int> Path) {
+    string Answer;
+    for(auto i=Path.begin();i!=Path.end();++i) {
+        string add = to_string(*i);
+        Answer.append(add);
+        Answer.append("-");
+    }
+    return Answer;
+}
 
+void Graph::resetbacktracking(int Final) {
+    vector<int> reset;
+    for(auto i=this->Nodes.begin();i!=this->Nodes.end();++i){
+        if(Final==i->getid()){
+            this->Nodes.at(Final-1).setgas(100);
+            this->Nodes.at(Final-1).path=reset;
+        }else{
+            continue;
+        }
+    }
+}
+
+int Graph::backtracking(int Init, int End,vector<int> UnvisitedNodes) {
+    vector<int> VisitedNodes;
+    map<int,int>::iterator itr;
+    for(itr= this->Nodes.at(Init).connections.begin();itr!=this->Nodes.at(Init).connections.end();++itr){
+        if((find(VisitedNodes.begin(),VisitedNodes.end(),itr->first)!=VisitedNodes.end())||(find(UnvisitedNodes.begin(),UnvisitedNodes.end(),itr->first)!=UnvisitedNodes.end())){//El nodo ya fue visitado o está siendo evaluado
+            continue;
+        }else if(itr->first-1==End){//El nodo por visitar es el objetivo
+            int ActualGas=this->Nodes.at(Init).getgas();//Capacidad que se requiere para llegar al nodo actual
+            int Endgas=this->Nodes.at(End).getgas();//Capacidad en el nodo
+            int Travelgas=itr->second;
+            if((ActualGas<Endgas)&&(Travelgas<Endgas)){//El camino requiere menot capacidad que el actual
+                this->Nodes.at(End).path=UnvisitedNodes;
+                VisitedNodes.push_back(End);
+                if(ActualGas<Travelgas) {//El camino del nodo hacia el final es la máxima capacidad requerida
+                    this->Nodes.at(End).setgas(Travelgas);
+                    continue;
+                }else{//El camino que requiere mayor capacidad se mantiene como el anterior
+                    this->Nodes.at(End).setgas(ActualGas);
+                    continue;
+                }
+            }else{//El nodo final tiene un cam mas corto
+                VisitedNodes.push_back(End);
+                continue;
+            }
+        }else{//El nodo No es el final se debe de evaluar
+            UnvisitedNodes.push_back(itr->first);
+            if(this->Nodes.at(Init).getgas()<itr->second){
+                this->Nodes.at(itr->first-1).setgas(itr->second);
+            }else{
+                this->Nodes.at(itr->first-1).setgas(this->Nodes.at(Init).getgas());
+            }
+            VisitedNodes.push_back(this->backtracking(itr->first-1,End,UnvisitedNodes)+1);
+            UnvisitedNodes.pop_back();
+        }
+    }
+    return Init;
+}
--- Backtracking/Graph.h
+++ Backtracking/Graph.h
@@ -10,20 +10,81 @@
 #include "NodeLogistic.h"
 #include "random"
 #include "string"
+#include "algorithm"
 
 using namespace std;
 
 class Graph {
 private:
-    vector<NodeLogistic> Nodes;
+    vector<NodeLogistic> Nodes;//Contiene todos los nodos
 
+    string Decitions;//Guarda todas las desiciones hechas por el algoritmo
+
+    int number;//Guarda el paso por el que va debuggeando las desiciones
+    /**
+ * Hace las conexiones pertinentes entre los nodos
+ * @param Node1 Uno de lnodos a conectar
+ * @param Node2 El otro nodo a conectar
+ * @param peso El valor de la conexion entre nodos
+ */
     void makeconnection(int Node1, int Node2, int peso);
 
-    NodeLogistic searchnode(int ID);
 
+    /**
+     * Genera automaticamente la conexion de uno nodo con todo el grafo
+     * Asegura que todo nodo va a estar conectado minimo una vez
+     * La probabilidad de conectar con otro nodo es de 25%
+     * Un nodo tiene un maximo de conexiones de n-1
+     * No puede conectar consigo mismo
+     * @param index
+     */
     void Adminconections(int index);
+
+    /**
+     * Modifica el parametro de gasolina del ultimo nodo para que
+     * sea maximo y siempre que se encuentre un camino viable se intercambie
+     * Ademas reinicia el path de el nodo
+     * @param Final
+     */
+    void resetbacktracking(int Final);
+
+    /**
+     * Funcion recursiva que evalua todos los nodos a profundidad
+     * Hace el return solo si ya recorrio todas las posibildades o
+     * si esta evaluando el nodo final, hace la revision en dado caso
+     * de que el camino envcontrado sea el de menor capacidad
+     * @param Init Nodo que se está evaluando
+     * @param End Nodo al que se desea llegar
+     * @param UnvisitedNodes Path para llegar a este nodo
+     * @return
+     */
+    int backtracking(int Init, int End,vector<int> UnvisitedNodes);
+
+    /**
+     * Convierte un path en un string
+     * @param Path
+     * @return Path hecho string
+     */
+    string StringifyAnswer(vector<int> Path);
+
 public:
     Graph(int size);
+
+    /**
+     * Dados un nodo de inicio y un nodo final devuelve un
+     * string con el camino de menor acidad para llegar a ese nodo
+     * @param Init Nodo de inicio
+     * @param Fin Nodo final
+     * @return Caminio recorrido
+     */
+    string Short(int Init, int Fin);
+
+    /**
+     * Busca un nodo en el grafo
+     * @param ID
+     * @return Nodo con el ID correspondiente
+     */
+    NodeLogistic searchnode(int ID);
 };
 
 
